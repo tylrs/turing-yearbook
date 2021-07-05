b4d34a3c992344d95bd457691b419f68a2523d91
Taylor Galloway <gallowaytaylor@outlook.com>
2021-07-05 16:16:01 -0600

    Add basic cohort structure

--- src/components/Cohort/Cohort.js
+++ src/components/Cohort/Cohort.js
@@ -2,6 +2,14 @@ import React from 'react';
 import Person from '../Person/Person';
 import './Cohort.css';
 
-// COHORT COMPONENT CODE GOES HERE
+const cohort = () => {
+    let cohort = props.staff.map(member => <Person key={member.id} info={member}/>)
+    return (
+        <main>
+            {cohort}
+        </main>
+    )
+        
+}
 
 export default Cohort;
