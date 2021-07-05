97905825ce09644ed9ed568f4461e48641d34757
Taylor Galloway <gallowaytaylor@outlook.com>
2021-07-05 16:34:43 -0600

    Render person info

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import './Person.css';
 
-const Person = () => {
-    
+const Person = (props) => {
+    console.log(props)
+    let {name, photo, quote, superlative} = props.info
+    return (
+        <section>
+            <img src={photo} alt={name}/>
+            <h3>{name}</h3>
+            <p>{quote}</p>
+            <p>{superlative}</p>
+        </section>
+    )
 }
 
 export default Person;
